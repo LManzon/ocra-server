127525c5cd6b383919896fb5fa0900d1b18316c1
Tadej <udovictadej@gmail.com>
2021-06-03 20:18:41 +0100

    Objectives model done

---
+++ models/Objectives.model.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require("mongoose");
+
+const objectivesSchema = new Schema({
+  problem: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ["Career", "Passion", "Relationship", "Financial", "Wellbeing"],
+    required: true,
+  },
+
+  objectiveInput: {
+    type: String,
+    required: true,
+  },
+
+  objectiveEndDate: {
+    type: Date,
+    required: true,
+  },
+
+  keyResult: {
+    type: String,
+    required: true,
+  },
+
+  action: {
+    type: [{ type: Schema.Types.ObjectId, ref: "Action" }],
+  },
+
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+
+  buddy: {
+    type: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    required: true,
+  },
+
+  status: {
+    type: String,
+    enum: ["Not Started", "In-Progress", "Completed"],
+    default: "Not Started",
+  },
+
+  visibility: {
+    type: String,
+    enum: ["Public", "Private", "Friends"],
+    default: "Private",
+  },
+
+  sharedWithUser: {
+    array: [],
+  },
+
+  usersResponse: {
+    type: Boolean,
+  },
+
+  comments: {
+    array: [],
+  },
+});
+
+const Objective = model("Objective", objectivesSchema);
+
+module.exports = Objective;

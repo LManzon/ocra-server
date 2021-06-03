8e07882c1286b8f237ff744a85352a3f39f5e588
Luca Manzon <manzonluca@gmail.com>
2021-06-03 21:18:24 +0200

    Actions and User models

---
+++ models/Actions.model.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const actionSchema = new Schema({
+  action: {
+    type: String,
+    required: true,
+  },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+
+  objectives: { type: Schema.Types.ObjectId, ref: "Objective" },
+
+  status: {
+    type: String,
+    enum: ["Not Started", "In-Progress", "Completed"],
+    default: "Not Started",
+  },
+
+  actionDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Action = model("User", actionSchema);
+
+module.exports = Action;

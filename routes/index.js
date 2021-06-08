583f3eb34b0f29ef2fb7ff0fd1510a0d7c7866d3
Luca Manzon <manzonluca@gmail.com>
2021-06-08 20:51:11 +0200

    cloudinary

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,18 @@ const router = require("express").Router();
 const authRoutes = require("./auth");
 const profileRouter = require("./profile");
 const Objectives = require("./Objectives");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "ocra-final-project",
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/", (req, res, next) => {
     res.json("All good in here");
